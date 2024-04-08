d77dcb7fd8481a88230a13b8c293c6f76e85fdb7
yeasinarafatprantik2002 <yeasinarafatprantik@gmail.com>
2024-04-08 13:33:47 +0600

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center  py-2 mt-10">
+      <div className="m-4 flex">
+        <h1 className="text-4xl font-bold underline">About Us</h1>
+      </div>
+
+      <div className="flex flex-wrap mx-8">
+        <p className=" text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          cupiditate autem sequi voluptate vel facilis enim ipsam voluptatum,
+          aliquam amet ratione, dolorem aliquid. Nihil distinctio explicabo
+          nesciunt sequi quae, est saepe recusandae maiores tempora delectus
+          provident ullam totam voluptatem similique eaque praesentium dicta
+          possimus error. Fugiat, obcaecati exercitationem explicabo corporis
+          quos ea fugit. Quis blanditiis ad porro inventore soluta assumenda
+          magnam expedita iste consequuntur, amet laborum omnis natus, ipsa
+          dolorum? Fugit blanditiis molestiae voluptatum fuga impedit neque
+          placeat, commodi libero. Voluptatem ut voluptatum dicta consequuntur.
+          Saepe quo quod temporibus dolore adipisci ad culpa quos quasi! Quasi,
+          corrupti magni ad minima corporis vitae pariatur dolor laboriosam odit
+          cum facere eaque fugiat veniam aliquam, aliquid incidunt rerum quos
+          velit! Facilis nisi ea assumenda nulla cumque excepturi voluptatum
+          totam harum illum impedit, dignissimos culpa cupiditate, quam ducimus,
+          sapiente distinctio omnis quas fugiat quaerat. Nihil, debitis officiis
+          possimus repellendus itaque tempore nesciunt distinctio libero
+          impedit. Inventore odio error minus et incidunt? Necessitatibus ut,
+          odit maiores est saepe blanditiis voluptas numquam libero odio amet
+          possimus officiis commodi incidunt voluptatibus illum ex nostrum quasi
+          earum nulla distinctio. Ipsam earum in ipsum nobis quaerat
+          necessitatibus laboriosam quisquam doloremque reprehenderit, eligendi,
+          perferendis minus incidunt sit vel dolor ad.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
